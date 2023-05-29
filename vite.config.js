e14f67078bda745952d64c82af8664e2b6705222
kamiviolet <kamiviolet@gmail.com>
2023-05-29 15:34:17 +0100

    adjust the config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-react/'
+  base: '/portfolio-react/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
