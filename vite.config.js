f6c965cf8f6dae23ce621b51841760766f57f1e6
kamiviolet <kamiviolet@gmail.com>
2023-05-29 15:36:17 +0100

    delete resolve in vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio-react/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
