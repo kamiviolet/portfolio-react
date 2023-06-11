0d38045fa18af9010ed11c9ca4e0806193e78017
kamiviolet <kamiviolet@gmail.com>
2023-06-11 09:53:57 +0100

    add carousel component for photos

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,8 @@
+export default function Carousel() {
+    return (
+        <figure className="intro_images" >
+            <img src="/intro_1.jpg" />
+            <figcaption>The kids of host family in Mongolia I met via CouchSurfing. (Photo taken in May, 2015.)</figcaption>
+        </figure>
+    )
+}
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,10 @@
+import "../css/about.css"
+import Carousel from "../components/Carousel"
+
 export default function About() {
     return (
-        <main>
-            <section className="section about">
+        <main role="about_page">
+            <section className="about">
                 <h2>Background</h2>
                 <p>
                     Being one of the nerds at school, I have been always grateful for the existence of computer and the internet, that I could access to information. I liked to explore materials to learn various softwares without waiting for my computer teacher to introduce to us. In early 2000s, I hosted my first homepage in Geocities JP. It was fun to think of various themes and designs to put on my homepage back then.
@@ -10,6 +13,7 @@ export default function About() {
                 <p>
                     Despite my passion about science, I am also fascinated by history and ended up in humanities. I studied Religious studies and Philosophy for my first degree and then pursued Japanese Studies for my postgraduate degree. I learnt to skim tons of information and make analysis out of it. I became good at it and mainly, enjoy it a lot.
                 </p>
+                <Carousel />
                 <p>
                     Theory cannot be totally extracted from practice. I resigned my office job and then travelled from Hong Kong through Siberia to the UK by land in 2015 summer. I wished to experience different culture and understand people more. I made connection with various encounters, working as waitress at busy restaurant, babysitter, carer, sale assistant at supermarket and machine operator in manufacturing sector.
                 </p>
