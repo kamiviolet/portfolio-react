095deed470a428bf9126979c98491f791dd161c6
kamiviolet <kamiviolet@gmail.com>
2023-06-17 23:26:15 +0100

    edit footer disclaimer and add icon from unDraw

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,21 @@
+import {ThemeContext} from '../contexts/ThemeContext'
+import { useContext } from 'react'
 import '../css/header.css'
 
 export default function Home() {
+    const {theme, setTheme} = useContext(ThemeContext)
+
     return (
         <>
-        <header className="header">  
-           Welcome to my portfolio, I'm
-            <h1>
-                <span className="name">Kami Lam</span>
-            </h1>
-            <span className="title">JavaScript Developer</span>
-            
+        <header className="header">
+            <div role="description">
+                <p>Welcome to my portfolio, I'm</p>
+                <h1>
+                    <span className="name">Kami Lam</span>
+                </h1>
+                <span className="title">JavaScript Developer</span>
+            </div>
+            <img src="/undraw_programmer_re_owql.svg"/> 
         </header>
 
         <main>
