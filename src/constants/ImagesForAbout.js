06ca0906438d40ffd210b3729b0f9fbf8f755e43
kamiviolet <kamiviolet@gmail.com>
2023-07-10 13:58:12 +0100

    minor adjustment

---
+++ src/constants/ImagesForAbout.js
@@ -0,0 +1,21 @@
+import image1 from '/photos/intro_1.jpg'
+import image2 from '/photos/intro_2.jpg'
+import image3 from '/photos/intro_3.jpg'
+import image4 from '/photos/intro_4.jpg'
+import image5 from '/photos/intro_5.jpg'
+import image6 from '/photos/intro_6.jpg'
+import image7 from '/photos/intro_7.jpg'
+import image8 from '/photos/intro_8.jpg'
+import image9 from '/photos/intro_9.jpg'
+
+export const images = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9
+];
