1daf0881c6c783ed34d2f42cdf0880ac0e88e279
kamiviolet <kamiviolet@gmail.com>
2023-07-10 20:19:59 +0100

    separate header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <div role="description">
+                <p>Welcome to my portfolio, I'm</p>
+                <h1>Kami Lam</h1>
+                <span className="title">JavaScript Developer</span>
+            </div>
+        </header>
+    )
+}
