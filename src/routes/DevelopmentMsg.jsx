b56581b279a4be27c5a412b05b83d26af6d8a2d6
kamiviolet <kamiviolet@gmail.com>
2023-07-13 22:07:18 +0100

    correct typo

--- src/routes/DevelopmentMsg.jsx
+++ src/routes/DevelopmentMsg.jsx
@@ -3,7 +3,7 @@ export default function DevelopmentMsg() {
         <div id="development_msg">
             <img src="/lazy_uranus.svg" alt="lazy_uranus"/>
             <h2>It's preparing...</h2>
-            <p>Opps, sorry, my orbit takes long so it is not that I am lazy</p>
+            <p>Oops, sorry, my orbit takes long so it is not that I am lazy</p>
         </div>
     )
 }
